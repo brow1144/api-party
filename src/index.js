d03ec44c8b0815aca37948b5af731d6fad103432
Kyle Brown <brow1144@purdue.edu>
2017-06-20 16:23:54 -0400

    Github api fully functioning

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 
 ReactDOM.render(
   <Router>
-    <App /> 
-  </Router>,
-  
+    <App />
+  </Router>, 
   document.getElementById('root'));
-
 registerServiceWorker();
